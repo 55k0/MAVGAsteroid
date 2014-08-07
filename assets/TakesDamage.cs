00e5c2b339dc4884c03d75bd11515e77ceaf5a24
Keith <55k0.webmaster@gmail.com>
2014-08-07 19:53:51 -0400

    Base Shields working

--- assets/TakesDamage.cs
+++ assets/TakesDamage.cs
@@ -1,39 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class TakesDamage : MonoBehaviour {
-
+	
 	public int health = 3;
 	public int damagedThreshold = 1;
-
+	public int maxShields = 5;
+	public float shields = 5;
+	public float shieldDelay = 1.0f;
+	public float shieldRegen = 0.1f;
+	private DateTime lastHit = DateTime.Now;
+	
 	public Sprite sprite;
 	public Sprite damagedSprite;
-
+	
 	SpriteRenderer spriteRenderer;
-
+	
 	bool dead;
-
+	
 	// Use this for initialization
 	void Start () {
 		dead = false;
 		spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
 		spriteRenderer.sprite = sprite;
 	}
-
-
+	
+	void FixedUpdate(){
+		
+		if(shields < maxShields) {
+			
+			if((DateTime.Now.Subtract(lastHit).Seconds > shieldDelay)) {
+				
+				shields += shieldRegen;
+				
+			}
+			
+		}else if(shields > maxShields) {
+			
+			shields = maxShields;
+			
+		}
+		
+	}
+	
 	void OnCollisionEnter2D (Collision2D col)
 	{
-		if (col.gameObject.name.Contains ("Laser") || col.gameObject.name.Contains ("HomingMissle")) 
+		if (isBullet(col)) 
 		{
-
-
-			health -= 1;
+			
+			lastHit = DateTime.Now;
+			
+			if(shields >= 1) {
+				
+				shields -= 1;
+				
+			}else {
+				
+				health -= 1;
+				
+			}
+			
 			if(health <= 0)
 				dead = true;
-
+			
 			if(health <= damagedThreshold)
 				spriteRenderer.sprite = damagedSprite;
-
+			
 			// get the average of the contact points.
 			Vector2 pos = new Vector2(0, 0);
 			foreach(ContactPoint2D p in col.contacts)
@@ -41,24 +74,42 @@ public class TakesDamage : MonoBehaviour {
 				pos += p.point;
 			}
 			pos /= col.contacts.Length;
-
-
+			
+			
 			GameObject cameraObj = GameObject.FindGameObjectWithTag ("MainCamera");
 			GameObject playerObj = GameObject.FindGameObjectWithTag ("Player");
-
+			
 			float distance = 
 				(new Vector2(playerObj.transform.position.x, playerObj.transform.position.y) - pos).magnitude;
-
+			
 			cameraObj.GetComponent<MainCamera>().Shake (2, distance);
-
+			
 			Instantiate (Resources.Load ("Explosion"), pos, transform.rotation);
 		}
 	}
-
+	
 	void LateUpdate()
 	{
 		if(dead)
 			Destroy (gameObject);
 	}
-
+	
+	bool isBullet(Collision2D col) {
+		
+		string[] bulletNames = new string[]{"Laser","HomingMissle"};
+		
+		for(int i=0;i<bulletNames.Length;i++) {
+			
+			if(col.gameObject.name.Contains(bulletNames[i])) {
+				
+				return true;
+				
+			}
+			
+		}
+		
+		return false;
+		
+	}
+	
 }
